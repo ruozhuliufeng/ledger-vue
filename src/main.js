3a36884bdbd6316cd9ed47c4a047f046b2aeb919
ruozhuliufeng <18852971173@63.com>
2022-07-28 14:20:19 +0800

    项目基本结构

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+import request from "@/axios";
+import global from './globalFun'
 
-Vue.config.productionTip = false
+Vue.use(Element)
+Vue.prototype.$axios = request
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
